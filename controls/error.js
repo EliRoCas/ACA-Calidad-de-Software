0ec90073b8a9f49d47e44c5c60827a3e5cbac996
Eli RoCas <elir_94@hotmail.com>
2026-05-07 15:27:07 -0500

    fix: align dynamic error prop type. It was typed as Object but expected Array

--- controls/error.js
+++ controls/error.js
@@ -1,5 +1,5 @@
 const DynamicError = {
-  props: { errors: { type: Object, required: true } },
+  props: { errors: { type: Array, required: true } },
   template: `
         <ul v-if="errors.length" class="dynamic-error">
           <li v-for="(e, idx) in errors" :key="idx">{{ e.message }}</li>
